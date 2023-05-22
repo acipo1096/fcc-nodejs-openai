c58bebb46ffd2dd85d7358e671a8adddee5aa49c
Alex Cipollone <alex.cipollone10@gmail.com>
2023-05-22 14:49:06 -0400

    added notes and fixed App mistake

--- nodejs-chatgpt/src/App.jsx
+++ nodejs-chatgpt/src/App.jsx
@@ -9,7 +9,16 @@ const configuration = new Configuration({
 // Create new instance of OpenAI API:
 const openai = new OpenAIApi(configuration);
 
-function() App {
+function App() {
+
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  // The message will hold info sent from the app to the AI
+  // The chats array keeps track of all messages sent by both parties (user and AI)
+  // isTyping variable notifies the user whether the bot is typing or not
+
   return(
     <main>
       <h1>Chat AI Tutorial</h1>
