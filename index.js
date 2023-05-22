a7b6a8a71af5bce2fb2b7173c09bf345c210e09a
Alex Cipollone <alex.cipollone10@gmail.com>
2023-05-22 14:14:13 -0400

    wow! chatbot is working. That's crazy

--- index.js
+++ index.js
@@ -4,28 +4,35 @@ import readline from "readline";
 // Build the OpenAI Configuration
 // Creates a new instance of the Configuration object
 const configuration = new Configuration({
-    organization: "Need to create account",
-    apiKey: "Need to create account"
+    organization: "org-KJP6A7rCJ356baNwD5SRzU1Y",
+    apiKey: "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"
 });
 
 // Create new instance of OpenAI API:
-const openai = new OpenAIApi(Configuration);
+const openai = new OpenAIApi(configuration);
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
 
 // Calls the createChatCompletion function that triggers an API endpoint
 // Each message is an object containing the role (who sent the message) - could be from the AI or the human user
 // The code prints the response from the AI
 
-openai
-    .createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: "Hello"}]
-    })
-    .then((res) => {
-        console.log(res.data.choices[0].message.content);
-    })
-    .catch((e) => {
-        console.log(e);
-    });
+// openai
+//     .createChatCompletion({
+//         model: "gpt-3.5-turbo",
+//         messages: [{role: "user", content: "Hello"}]
+//     })
+//     .then((res) => {
+//         console.log(res.data.choices[0].message.content);
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     });
 
 /*
 About .then(res)
@@ -34,4 +41,24 @@ Meanwhile fetch returns a Promise.
 Promise helps with the asynchronous part and runs the function passed into then (res => res.json()) once the resource is loaded with the fetched resource as parameter. The fetched resource can be parsed using json() if it is JSON formatted.
 then also returns a Promise making it chainable.
 */
-    
+
+// The above function is not interactive
+// Below will create an interactive chat box
+
+// Prompts user to enter a message
+userInterface.prompt();
+
+userInterface.on("line", async (input) =>{
+    await openai
+        .createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: input}]
+        })
+        .then((res) => {
+            console.log(res.data.choices[0].message.content);
+            userInterface.prompt();
+        })
+        .catch((e) => {
+            console.log(e)
+    });
+});
